3e46242be8ab50622ec2ea14cae872fd5461dbe3
Max Helmetag <mhelmetag@gmail.com>
2020-03-14 22:17:58 -0700

    remove most subscriber stuff

--- app/models/lift.py
+++ app/models/lift.py
@@ -13,11 +13,20 @@ class Lift(Base):
     kind = Column(String, nullable=False)
     last_updated = Column(DateTime, nullable=False)
 
-    def _asdict(self):
+    def _for_html(self):
         return {
             'id': self.id,
             'name': self.name,
             'status': self.status,
             'kind': self.kind,
             'last_updated': self.last_updated.replace(tzinfo=gettz('UTC')).astimezone(tz=gettz('America/Los_Angeles')).strftime('%-I:%M %p %Z')
+        }
+
+    def _for_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'status': self.status,
+            'kind': self.kind,
+            'last_updated': self.last_updated.replace(tzinfo=gettz('UTC')).isoformat()
         }
--- web.py
+++ web.py
@@ -7,68 +7,26 @@ from starlette.templating import Jinja2Templates
 from starlette.responses import JSONResponse, RedirectResponse
 from starlette.routing import Mount
 from starlette.staticfiles import StaticFiles
-from starlette.responses import FileResponse
 
 templates = Jinja2Templates(directory='app/templates')
-
 routes = [Mount('/static', app=StaticFiles(directory='app/static'), name="static")]
-
 app = Starlette(routes=routes)
 
 @app.route('/', methods=['GET', 'POST'])
 async def root(request):  
   session = Session()
   lifts = session.query(Lift).all()
-  lift_dicts = [l._asdict() for l in lifts]
+  lift_dicts = [l._for_html() for l in lifts]
 
   session.close()
 
   return templates.TemplateResponse('lifts/index.html', {'request': request, 'lifts': lift_dicts})
 
-@app.route('/favicon.ico', methods=['GET'])
-async def favicon(request):
-  return FileResponse('app/static/favicon.ico')
-
-@app.route('/subscribe', methods=['GET'])
-async def subscribe(request):  
-  return templates.TemplateResponse('subscribers/new.html', {'request': request})
-
-@app.route('/subscribe', methods=['POST'])
-async def post_subscribe(request):
-  form = await request.form()
-  phone_number = form['phone_number']
-
-  session = Session()
-  subscriber = Subscriber(phone_number=phone_number)
-  session.add(subscriber)
-  session.commit()
-  session.close()
-
-  return RedirectResponse(url='/')
-
-@app.route('/unsubscribe', methods=['GET'])
-async def unsubscribe(request):  
-  return templates.TemplateResponse('subscribers/delete.html', {'request': request})
-
-@app.route('/unsubscribe', methods=['POST'])
-async def post_unsubscribe(request):
-  form = await request.form()
-  phone_number = form['phone_number']
-
-  session = Session()
-  subscriber = session.query(Subscriber).filter(Subscriber.phone_number == phone_number).one()
-  session.delete(subscriber)
-  session.commit()
-  session.close()
-
-  return RedirectResponse(url='/')
-
-
-@app.route('/lifts', methods=['GET'])
+@app.route('/api/lifts', methods=['GET'])
 async def lifts(request):
   session = Session()
   lifts = session.query(Lift).all()
-  lift_dicts = [l._asdict() for l in lifts]
+  lift_dicts = [l._for_json() for l in lifts]
 
   session.close()
 
