d80c97331cd2457e1acdab8274634bc59d9a8ce3
Max Helmetag <mhelmetag@gmail.com>
2020-03-14 17:21:01 -0700

    timezone nonsense

--- web.py
+++ web.py
@@ -1,6 +1,8 @@
 from app.models.lift import Lift
 from app.db.session import Session
 
+from dateutil.tz import gettz
+
 from starlette.applications import Starlette
 from starlette.templating import Jinja2Templates
 
@@ -10,7 +12,9 @@ app = Starlette()
 
 @app.route('/', methods=['GET'])
 async def root(request):
+  timezone = gettz('America/Los_Angeles')
+
   session = Session()
   lifts = session.query(Lift).all()
 
-  return templates.TemplateResponse('lifts/index.html', {'request': request, 'lifts': lifts})
+  return templates.TemplateResponse('lifts/index.html', {'request': request, 'timezone': timezone, 'lifts': lifts})
