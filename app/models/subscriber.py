db351887c48d0b6a7b4a8361ca33691b3471d366
Max Helmetag <mhelmetag@gmail.com>
2020-03-14 19:04:01 -0700

    add fake service for testing and updater

---
+++ app/models/subscriber.py
@@ -0,0 +1,15 @@
+from app.models.base import Base
+
+from sqlalchemy import Column, Integer, String
+
+class Subsciber(Base):
+    __tablename__ = 'subscribers'
+
+    id = Column(Integer, primary_key=True)
+    phone_number = Column(String, nullable=False, unique=True)
+
+    def _asdict(self):
+        return {
+            'id': self.id,
+            'phone_number': self.phone_number
+        }
--- web.py
+++ web.py
@@ -3,6 +3,7 @@ from app.db.session import Session
 
 from starlette.applications import Starlette
 from starlette.templating import Jinja2Templates
+from starlette.responses import JSONResponse
 
 templates = Jinja2Templates(directory='app/templates')
 
@@ -17,3 +18,13 @@ async def root(request):
   session.close()
 
   return templates.TemplateResponse('lifts/index.html', {'request': request, 'lifts': lift_dicts})
+
+@app.route('/lifts', methods=['GET'])
+async def lifts(request):
+  session = Session()
+  lifts = session.query(Lift).all()
+  lift_dicts = [l._asdict() for l in lifts]
+
+  session.close()
+
+  return JSONResponse({'lifts': lift_dicts})
