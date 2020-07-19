235289d25d190fdc9af86c4180dbb14d1525149c
Max Helmetag <mhelmetag@gmail.com>
2020-07-18 18:26:01 -0700

    remove weirdo jquery-ish $ from navbar js

--- app/static/navbar.js
+++ app/static/navbar.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const $navbarBurgers = Array.prototype.slice.call(
+  const navbarBurgers = Array.prototype.slice.call(
     document.querySelectorAll(".navbar-burger"),
     0
   );
 
-  if ($navbarBurgers.length > 0) {
-    $navbarBurgers.forEach(el => {
+  if (navbarBurgers.length > 0) {
+    navbarBurgers.forEach((el) => {
       el.addEventListener("click", () => {
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
+        const targetId = el.dataset.target;
+        const target = document.getElementById(targetId);
 
         el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
+        target.classList.toggle("is-active");
       });
     });
   }
